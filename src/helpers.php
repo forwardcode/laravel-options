60aab3797d6ce23f8b0f346506d5914baa1ae00c
Rahul Gupta <fc.rahulgupta@gmail.com>
2022-07-08 22:53:54 +0530

    Composer updated.

--- src/helpers.php
+++ src/helpers.php
@@ -2,6 +2,21 @@
 
 if (!function_exists('get_option')) {
     /**
+     * Get the specified option value.
+     *
+     *
+     * @param  array|string  $key
+     * @param  mixed  $default
+     * @return mixed|\Forwardcode\LaravelOptions\Model\Option
+     */
+    function get_option($key = null, $default = null)
+    {
+        return app('option')->get($key, $default);
+    }
+}
+
+if (!function_exists('set_option')) {
+    /**
      * Get / set the specified option value.
      *
      * If an array is passed as the key, we will assume you want to set an array of values.
@@ -10,28 +25,20 @@ if (!function_exists('get_option')) {
      * @param  mixed  $default
      * @return mixed|\Appstract\Options\Option
      */
-    function get_option($key = null, $default = null)
+    function set_option($key = null, $default = null)
     {
-        if (is_null($key)) {
-            return app('option');
-        }
-
-        if (is_array($key)) {
-            return app('option')->set($key);
-        }
-
-        return app('option')->get($key, $default);
+        return app('option')->set($key, $default);
     }
 }
 
-if (!function_exists('get_option_exists')) {
+if (!function_exists('option_exists')) {
     /**
      * Check the specified option exits.
      *
      * @param  string  $key
      * @return mixed
      */
-    function get_option_exists($key)
+    function option_exists($key)
     {
         return app('option')->exists($key);
     }
